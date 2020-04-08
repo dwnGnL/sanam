83ebcb1677b0cb10d1c1d4624c905b5448c752be
dwnGnL <992938550845@yandex.ru>
2020-04-09 01:41:23 +0500

    add changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-var swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    effect: 'fade',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      }
-  });
+ var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      effect:"fade",
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	 		loop: true,
+			loopedSlides: 4
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      centeredSlides: true,
+      slidesPerView: 3,
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+			loop: true,
+			loopedSlides: 4
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
