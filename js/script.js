0e09166d09a270ea8b271723479a5aa826e72d49
dwnGnL <992938550845@yandex.ru>
2020-04-12 12:27:39 +0500

    add changes

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@
     });
     galleryTop.controller.control = galleryThumbs;
     galleryThumbs.controller.control = galleryTop;
+
+
+    var swiper = new Swiper('.fade-gallery', {
+      spaceBetween: 30,
+      effect: 'fade',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
