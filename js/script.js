7c9882da92f7298a908079dc800f653980984df6
dwnGnL <992938550845@yandex.ru>
2020-04-15 00:23:56 +0500

    add

--- js/script.js
+++ js/script.js
@@ -29,8 +29,8 @@
         clickable: true,
       },
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next1',
+        prevEl: '.swiper-button-prev1',
       },
     });
 
