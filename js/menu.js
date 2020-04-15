6880b281739213fb7635b701a7de0d42de29d696
DenzelHawking <denzelhawking@gmail.com>
2020-04-15 10:57:57 +0500

    header adaptived

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('.header__list');
+let menuButton = document.querySelector('.menu-button');
+
+menuButton.onclick = () => {
+    menuButton.classList.toggle('active');
+
+    if (menuButton.classList.contains('active')) {
+        menu.classList.add('is-open');
+        setTimeout(() => menu.classList.add('show-menu'), 0);
+    } else {
+        menu.classList.remove('show-menu');
+        setTimeout(() => menu.classList.remove('is-open'), 300);
+    };
+};
