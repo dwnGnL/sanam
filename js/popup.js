cf49c0ac98de8a811a3ffe8255c2768d98f4af07
DenzelHawking <denzelhawking@gmail.com>
2020-04-25 21:16:15 +0500

    popups added

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+// show-description__button
+// popup__desc
+
+let goodsItemqwe = document.querySelectorAll('.show-description__button');
+let popupDesc = document.querySelector('.popup__desc');
+
+goodsItemqwe.forEach((elem, index) => {
+  // console.log(index);
+  console.log(elem);
+  
+  elem.onclick = function() {
+  }
+});
